1b030325d5381eafbfd59863ebf3184684ffca9f
Efrain Arturo Gomez Fajardo <gom21023@byui.edu>
2022-02-09 12:18:39 -0700

    Finished object

--- Game/Jumper.cs
+++ Game/Jumper.cs
@@ -1,4 +1,46 @@
-class Jumper
+public class Jumper
 {
-    
+    TerminalService ts = new TerminalService();
+    // Draws the chute depending on the guesses
+    public void drawChute(int guess) 
+    {
+        if (guess == 4)
+        {
+            ts.WriteText(@"   ___  ");
+            ts.WriteText(@"  /___\ ");
+            ts.WriteText(@"  \   / ");
+            ts.WriteText(@"   \ /  ");
+            ts.WriteText(@"    X   ");
+            ts.WriteText(@"^^^^^^^^");
+        }
+        else if (guess == 3)
+        {
+            ts.WriteText(@"  /___\ ");
+            ts.WriteText(@"  \   / ");
+            ts.WriteText(@"   \ /  ");
+            ts.WriteText(@"    X   ");
+            ts.WriteText(@"^^^^^^^^");
+
+        }
+        else if (guess == 2)
+        {
+
+            ts.WriteText(@"  \   / ");
+            ts.WriteText(@"   \ /  ");
+            ts.WriteText(@"    X   ");
+            ts.WriteText(@"^^^^^^^^");
+        }
+        else if (guess == 1)
+        {
+            ts.WriteText(@"   \ /  ");
+            ts.WriteText(@"    X   ");
+            ts.WriteText(@"^^^^^^^^");
+
+        }
+        else
+        {
+            ts.WriteText(@"    X   ");
+            ts.WriteText(@"^^^^^^^^");
+        }
+    }
 }
