e2124ed61e46ca97c39fdf88a2742c76e58ec13b
Efrain Arturo Gomez Fajardo <gom21023@byui.edu>
2022-02-09 12:19:28 -0700

    Gives clues and still working on comparing guess

--- Game/Puzzle.cs
+++ Game/Puzzle.cs
@@ -1,14 +1,42 @@
-class Puzzle
+public class Puzzle
 {
     private string[] wordList = {"mosaic", "bounce", "wear", "freshman", "writer", "scatter", 
     "provide", "withdrawal", "launch", "pull"};
 
-    private int guess = 3;
+    private string[] wrongGuesses;
+    private string[] correctGuesses; 
+    public int guessNum = 4;
+    public string word = "";
+    TerminalService ts = new TerminalService();
 
-    public string getWord()
+    public Puzzle()
     {
         Random random = new Random();
-        string word = wordList[random.Next(0, wordList.Length - 1)];
-        return word;
+        word = wordList[random.Next(0, wordList.Length - 1)];
     }
+
+    public void printWord()
+    {
+        for (int i = 0; i < word.Length; i++)
+        {
+            Console.Write("_ ");
+        }
+    }
+    // Compares the user's input with the actual word
+   public void compareGuess(string guess)
+    {
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (word[i].ToString() == guess)
+            {
+                
+            ts.WriteText("It is present");
+            
+            }
+        }
+    }
+
+    // Tells if the correct guesses are equal to the actual word
+
+
 }
